023e9f8d4d2fd143dc1e551f948cfb1dbe42f5ef
viktor-kov <viktor.kovacs02@gmail.com>
2021-02-22 23:59:47 +0100

    user role update validacia (server)

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRoleUpdateRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserRoleController extends Controller
 {
-    public function update(Request $request) {
+    public function update(UserRoleUpdateRequest $request) {
         
         if($request['role'] == 0 || $request['role'] == 1 ) {
-            $id = $request['id'];
-            $role = $request['role'];
+            $id = $request->id;
+            $role = $request->role;
 
             $update_user = User::where('id', $id)->update(['current_team_id' => $role]);
     
             return back();
         }
+
+        return back();
     }
 }
