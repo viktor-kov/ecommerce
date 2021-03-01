535ef18a44b22c23244f02e28ecdcc319d8d76ed
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-01 19:08:21 +0100

    refresh objednavok kazdych 5min

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.adminlayout')
 
+@section('extra-js')
+<script>
+    $(document).ready(function(){
+        setInterval(function() {
+            $( "#order-list" ).load( "{{route('orders')}} #order-list li" );
+        }, 300000);
+    });
+</script>
+@endsection
+
 @section('stats')
 
 <h1 class="text-center text-3xl">{{__('admin.orders')}}</h1>
-<ul>
+<ul id="order-list">
     @foreach ($orders as $order)
     <li class="bg-gray-300  p-2 border border-black mb-1">
         <p>{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $order->created_at)->format('H:i | d.m.Y') }}</p>
