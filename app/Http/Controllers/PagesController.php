a37eb4b1c17974fa47ba769faef646dcfb3feed1
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-06 17:10:08 +0100

    deleted the old forgotten view return and deleted the specification view return

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -136,60 +136,47 @@ class PagesController extends Controller
     }
 
     public function edit_product($id) {
-         //select product by slug from db
-         $product = Product::where('id', $id)->first();
-         //get the product id
-         $product_id = $product->id;
-         //get the product category
-         $product_category = $product->category;
+        //select product by slug from db
+        $product = Product::where('id', $id)->first();
+        //get the product id
+        $product_id = $product->id;
+        //get the product category
+        $product_category = $product->category;
 
-         switch($product_category) {
-             case 1:
-                 $product_specifications = MemorySpecification::where('product_id', $product_id)->first();
-                 $specification_view = 'specifications.ram';
-                 break;
+        switch($product_category) {
+            case 1:
+                $product_specifications = MemorySpecification::where('product_id', $product_id)->first();
+                break;
              case 2:
-                 $product_specifications = CpuSpecification::where('product_id', $product_id)->first();
-                 $specification_view = 'specifications.cpu';
-                 break;
+                $product_specifications = CpuSpecification::where('product_id', $product_id)->first();
+                break;
              case 3:
-                 $product_specifications = MotherboardSpecification::where('product_id', $product_id)->first();
-                 $specification_view = 'specifications.motherboard';
-                 break;
+                $product_specifications = MotherboardSpecification::where('product_id', $product_id)->first();
+                break;
              case 4:
-                 $product_specifications = CaseSpecification::where('product_id', $product_id)->first();
-                 $specification_view = 'specifications.case';
-                 break;
+                $product_specifications = CaseSpecification::where('product_id', $product_id)->first();
+                break;
              case 5:
-                 $product_specifications = SupplySpecification::where('product_id', $product_id)->first();
-                 $specification_view = 'specifications.supply';
-                 break;
+                $product_specifications = SupplySpecification::where('product_id', $product_id)->first();
+                break;
              case 6:
-                 $product_specifications = DiskSpecification::where('product_id', $product_id)->first();
-                 $specification_view = 'specifications.disk';
-                 break;
+                $product_specifications = DiskSpecification::where('product_id', $product_id)->first();
+                break;
              case 7:
-                 $product_specifications = CoolingSpecification::where('product_id', $product_id)->first();
-                 $specification_view = 'specifications.cooling';
-                 break;
+                $product_specifications = CoolingSpecification::where('product_id', $product_id)->first();
+                break;
              case 8:
-                 $product_specifications = GpuSpecification::where('product_id', $product_id)->first();
-                 $specification_view = 'specifications.gpu';
-                 break;
-         }
-
-         return view('edit_product', [
-             'product' => $product,
-             'product_id' => $product_id,
-             'product_category' => $product_category,
-             'product_specifications' => $product_specifications,
-             'specification_view' => $specification_view,
-         ]);
-
+                $product_specifications = GpuSpecification::where('product_id', $product_id)->first();
+                break;
+        }
 
         return view('edit_product', [
-            'product' => Product::where('id', $id)->first(),
+            'product' => $product,
+            'product_id' => $product_id,
+            'product_category' => $product_category,
+            'product_specifications' => $product_specifications,
         ]);
+
     }
     
     public function thankyou() {
