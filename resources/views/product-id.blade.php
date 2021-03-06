11335fd29ff20b1d90970cd784cd40572e62e51a
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-06 23:49:48 +0100

    styled the add to cart buttont and the product description

--- resources/views/product-id.blade.php
+++ resources/views/product-id.blade.php
@@ -3,22 +3,22 @@
 
 @section('content')
     <div class="main-container shadow-xl">
-        <section class="grid grid-cols-2 mt-4">
+        <section class="grid grid-cols-2 mt-4 p-8">
             <img src="{{asset('img/'.$product->photo_path)}}" alt="" class="h-200">
-            <div class="product-informations m-5">
+            <div class="product-informations">
                 <h1 class="text-4xl">{{$product->name}}</h1>
-                <p class="mt-4 h-64 overflow-y-auto p-2 border-l bg-gray-100 shadow-xl">{{$product->text}}</p>
-                <aside class="pricin pt-6 pb-6">
-                    <p class="flex justify-between text-gray-400">{{__('products.with-dph')}} <span>{{$product->price}}€</span></p>
+                <p class="mt-4 h-64 overflow-y-auto p-2 border border-dashed">{{$product->text}}</p>
+                <aside class="pricin mt-6 mb-6 p-2 bg-gray-200">
+                    <p class="flex justify-between">{{__('products.with-dph')}} <span class="font-extrabold text-xl">{{$product->price}}€</span></p>
                     <p class="flex justify-between text-gray-400">{{__('products.without-dph')}} <span>{{$product->without_dph}}€</span></p>
                 </aside>
-                <aside class="buy-buttons flex justify-evenly">
+                <aside class="buy-buttons">
                     <form action="{{route('cart.store')}}" method="post">
                         @csrf
                         <input type="hidden" name="product_id" value="{{$product->slug}}">
                         <input type="hidden" name="product_name" value="{{$product->name}}">
                         <input type="hidden" name="product_category" value="{{$product->category}}">
-                        <input type="submit" value="{{__('products.add-to-cart')}}" class="p-4 bg-green-400 text-white cursor-pointer">
+                        <input type="submit" value="{{__('products.add-to-cart')}}" class="p-4 bg-green-400 text-white cursor-pointer w-full block">
                     </form>
                 </aside>
             </div>
@@ -32,7 +32,7 @@
 
 
 @section('often-bought')
-<ul class="hidden md:grid md:grid-cols-2 lg:grid-cols-4 gap-x-2 mt-10">
+<ul class="hidden md:grid md:grid-cols-2 lg:grid-cols-4 gap-x-2 mt-10 mb-10">
     @foreach ($featured_products as $featured_product) 
         <li class="product-container bg-gray-200 shadow-xl h-100">
             <a href="{{route('product.show', $featured_product->category)}}/{{$featured_product->slug}}">
