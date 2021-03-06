26a59fd47ee56b5f9177a471b5c9aa49b068c0c0
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-06 16:49:44 +0100

    modified the ajax url for specifications in edit page

--- resources/views/edit_product.blade.php
+++ resources/views/edit_product.blade.php
@@ -2,6 +2,11 @@
 
 @section('extra-js')
     <script>
+        window.onload = function() {
+            url = 'http://localhost/www/ecommerce/public/specifications/' + {{$product_category}} + '/' + {{$product_id}};
+            $("#parameters").load(url);
+        } 
+
         function show(selected) {
             let product_category = selected.value;
             let url = 'http://localhost/www/ecommerce/public/specifications/' + product_category;
