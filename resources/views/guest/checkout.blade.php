f68429c425e01dfe9b0be561ccff00914db49d72
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-17 20:52:25 +0100

    fixed image bug on checkout page

--- resources/views/guest/checkout.blade.php
+++ resources/views/guest/checkout.blade.php
@@ -63,7 +63,7 @@
                         <li class="mb-2 flex border-b border-black">
                             <section class="grid grid-cols-4 justify-around p-2 text-xl w-full">
                                 <a href="{{route('product.show', $product->options->category)}}/{{$product->id}}">
-                                    <img src="{{asset('img/products/'.$product->id.'.jpeg')}}"  alt="{{$product->name}}" class="h-24 w-auto">
+                                    <img src="{{asset('img/'.$product->options->product_photo)}}"  alt="{{$product->name}}" class="h-24 w-auto">
                                 </a>
                                 <h1 class="grid items-center"><a href="{{route('product.show', $product->options->category)}}/{{$product->id}}">{{$product->name}}</a></h1>
                                 <h1 class="place-self-center">{{$product->qty}}</h1>
