45fab33def7183c59cd3c934d7d3e47e7509fb4f
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-19 16:59:42 +0100

    added notes to invoice controller

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -8,9 +8,11 @@ use Barryvdh\DomPDF\Facade as PDF;
 use Illuminate\Support\Facades\Storage;
 
 class InvoiceController extends Controller
-{
+{   
+    //show the requested invoice
     public function show($id) {
         
+        //only  the logged in users can see their invoices
         if(auth()->user()) {
             $invoice = Invoice::where('invoice_name', $id)->first();
 
@@ -26,6 +28,7 @@ class InvoiceController extends Controller
         }
     }
 
+    //update the invoice status
     public function updateStatus($id) {
         $invoice = Invoice::where('id', $id)->update(['active' => 0]);
         return redirect()->route('orders')->with('success', 'Objednávka vybavená!');
