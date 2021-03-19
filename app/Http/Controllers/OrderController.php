901e708782ccf9b99efbcea5d086c8bc13f82e00
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-19 17:04:34 +0100

    added notes to order controller + findOrFail for invoice

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+    //show the order by id in invoice table
     public function showOrder($id) {
+        //find or fail to find the invoice
+        Invoice::findOrFail($id);
+        //getting all the products in order to display them
         $all_products = Order::all()->where('invoice_id', $id);
         
+        //calculating the price
         $total_sum = 0;
         foreach($all_products as $product) {
             $price = $product->price * $product->quantity;
