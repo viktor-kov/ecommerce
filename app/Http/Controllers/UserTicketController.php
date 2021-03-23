5de78d1e8c440ecad291f6ff929fd32798fdd48c
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-23 18:43:57 +0100

    added notes

--- app/Http/Controllers/UserTicketController.php
+++ app/Http/Controllers/UserTicketController.php
@@ -9,61 +9,81 @@ use App\Events\TicketMessageEvent;
 
 class UserTicketController extends Controller
 {
-    //
-
+    //show all tickets to admin or all tickets belongs to logged in user
     public function index() {
         if(auth()->user()->current_team_id == 1) {
             $tickets = UserTicket::where('status', 1)->orderBy('created_at', 'DESC')->get();
+            return view('ticket.ticket-index', ['tickets' => $tickets]);
         }
-        else {
-            $tickets = UserTicket::where('user_id', auth()->id())->orderBy('created_at', 'DESC')->get();
-        }
+
+        $tickets = UserTicket::where('user_id', auth()->id())->orderBy('created_at', 'DESC')->get();
         return view('ticket.ticket-index', ['tickets' => $tickets]);
     }
 
+    //new ticket
     public function newTicket() {
         return view('ticket.ticket-new');
     }
 
+    //save the new ticket
     public function saveTicket(Request $request) {
 
         $ticket_title = $request->ticket_title;
         $ticket_message = $request->ticket_text;
 
+        //creating the new ticket
         $new_ticket = UserTicket::create([
             'title' => $ticket_title,
             'user_id' => auth()->id(),
         ]);
-
+        
+        //saving the ticket message
         TicketMessage::create([
             'ticket_id' => $new_ticket->id,
             'user_id' => auth()->id(),
             'ticket_message' => $ticket_message,
         ]);
-
+        
+        //redireckting to the ticket
         return redirect()->route('ticket.show', ['ticket_id' => $new_ticket->id]);
     }
 
+    //showing the ticket
     public function showTicket($ticket_id) {
-        $ticket_status = UserTicket::findOrFail($ticket_id)->status;
+        //finding the ticket if exists
+        $ticket = UserTicket::findOrFail($ticket_id);
 
+        //if the ticket is not belongs to logged in user or we are not a admin, show 403 error
+        if($ticket->user_id != auth()->id() && auth()->user()->current_team_id != 1)
+        {
+            abort(403);
+        }
+
+        //ticket status
+        $ticket_status = $ticket->status;
+
+        //retrieving all messages in ticket
         $messages = TicketMessage::all()->where('ticket_id', $ticket_id);
         return view('ticket.ticket', ['messages' => $messages, 'ticket_id' => $ticket_id, 'ticket_status' => $ticket_status]);
     }
 
+    //saving ticket message
     public function storeTicketMesage($ticket_id, Request $request) {
         $message = $request->ticket_text;
 
+        //inserting the message to db
         TicketMessage::create([
             'ticket_id' => $ticket_id,
             'user_id' => auth()->id(),
             'ticket_message' => $message,
         ]);
         
+        //creating the event
         event(new TicketMessageEvent($message, $ticket_id));
         return back();
     }
 
+    //updating the ticket status
     public function ticketStatus($ticket_id) {
         UserTicket::where('id', $ticket_id)->update([
             'status' => '0',
