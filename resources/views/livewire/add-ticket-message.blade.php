4a7ba15064e6e05e11fda718a836d3f6779f91a3
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-24 12:29:40 +0100

    added form csrf token for preventing future problems

--- resources/views/livewire/add-ticket-message.blade.php
+++ resources/views/livewire/add-ticket-message.blade.php
@@ -1,5 +1,6 @@
 <div>
-    <form wire:submit.prevent="saveMessage">
+    <form wire:submit.prevent="saveMessage" action="#" method="POST">
+        @csrf
         <textarea name="ticket_text" rows="10" class="w-full border border-black p-2" wire:model="ticket_text"></textarea>
         <button type="submit" class="p-2 bg-green-500 text-white block mt-2 cursor-pointer w-full">Submit</button>
     </form>
