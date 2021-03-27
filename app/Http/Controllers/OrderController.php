a789985e4d6ee8a989a01cefaaa65ea1c224a5c0
viktor-kov <viktor.kovacs02@gmail.com>
2021-03-27 17:43:11 +0100

    better sum for all products price

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Invoice;
 use App\Models\Order;
+use App\Models\Invoice;
 use Illuminate\Http\Request;
+use Stripe\Service\OrderService;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -12,19 +14,16 @@ class OrderController extends Controller
     public function showOrder($id) {
         //find or fail to find the invoice
         Invoice::findOrFail($id);
+
         //getting all the products in order to display them
         $all_products = Order::all()->where('invoice_id', $id);
         
-        //calculating the price
-        $total_sum = 0;
-        foreach($all_products as $product) {
-            $price = $product->price * $product->quantity;
-            $total_sum += $price;
-        }
+        //total sum of all products in order
+        $all_products_price = Order::where('invoice_id', $id)->sum(DB::raw('price * quantity'));
 
         return view('admin.show-order', [
             'order' => $all_products,
-            'total_sum' => $total_sum,
+            'total_sum' => $all_products_price,
         ]);
     }
 }
