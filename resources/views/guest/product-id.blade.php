445bf198d623f947f784dfa7fe1e88b28c53f28a
viktor-kov <viktor.kovacs02@gmail.com>
2021-04-03 13:02:40 +0200

    img fix

--- resources/views/guest/product-id.blade.php
+++ resources/views/guest/product-id.blade.php
@@ -23,7 +23,7 @@
 @section('content')
     <div class="main-container shadow-xl">
         <section class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 mt-4 p-8">
-            <img src="{{asset('img/'.$showed_product->photo_path)}}" alt="" class="h-200 mx-auto">
+            <img src="{{asset('img/'.$showed_product->photo_path)}}" alt="" class="h-200 mx-auto p-2">
             <div class="product-informations">
                 <h1 class="text-4xl">{{$showed_product->name}}</h1>
                 <p class="mt-4 h-64 overflow-y-auto p-2 border border-dashed">{{$showed_product->text}}</p>
