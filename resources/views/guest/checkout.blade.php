25eedaab5a78971eeeb764037997220cc57513d0
viktor-kov <viktor.kovacs02@gmail.com>
2021-04-12 22:46:18 +0200

    fix

--- resources/views/guest/checkout.blade.php
+++ resources/views/guest/checkout.blade.php
@@ -62,10 +62,14 @@
                     @foreach (Cart::content() as $product)
                         <li class="mb-2 flex border-b border-black">
                             <section class="grid grid-cols-4 justify-around p-2 text-xl w-full">
-                                <a href="{{route('product.show', $product->options->category)}}/{{$product->id}}">
+                                <a href="{{route('product.show', ['id' => $product->options->category, 'slug' => $product->options->slug ])}}">
                                     <img src="{{asset('img/'.$product->options->product_photo)}}"  alt="{{$product->name}}" class="h-24 w-auto">
                                 </a>
-                                <h1 class="grid items-center"><a href="{{route('product.show', $product->options->category)}}/{{$product->id}}">{{$product->name}}</a></h1>
+                                <h1 class="grid items-center">
+                                    <a href="{{route('product.show', ['id' => $product->options->category, 'slug' => $product->options->slug ])}}">
+                                        {{$product->name}}
+                                    </a>
+                                </h1>
                                 <h1 class="place-self-center">{{$product->qty}}</h1>
                                 <h2 class="place-self-center">{{number_format($product->price, 2)}}€</h2>
                             </section>
