2861a2617ddee766cb1556fd1c00cc6bf08b92d7
viktor-kov <viktor.kovacs02@gmail.com>
2021-04-13 08:38:50 +0200

    orders status bar fix

--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -20,29 +20,29 @@
 
 @section('stats')
 <section>
-    <ul class="grid grid-cols-4 text-center text-xl p-2 bg-green-400">
-        <li>
+    <ul class="grid grid-cols-1 md:grid-cols-4 text-center text-xl p-1">
+        <li class="mb-1 bg-green-400 p-2">
             @if (request()->segment(count(request()->segments())) == "show")
                 <strong><a href="{{route('orders')}}">{{__('order.order-active')}}</a></strong>
             @else 
                 <a href="{{route('orders')}}">{{__('order.order-active')}}</a>
             @endif
         </li>
-        <li>
+        <li class="mb-1 bg-green-400 p-2">
             @if (request()->segment(count(request()->segments())) == 200)
                 <strong><a href="{{route('orders.category', ['category' => 200])}}">{{__('order.packed')}}</a></strong>
             @else 
                 <a href="{{route('orders.category', ['category' => 200])}}">{{__('order.packed')}}</a>
             @endif
         </li>
-        <li>
+        <li class="mb-1 bg-green-400 p-2">
             @if (request()->segment(count(request()->segments())) == 300)
                 <strong><a href="{{route('orders.category', ['category' => 300])}}">{{__('order.shipped')}}</a></strong>
             @else 
                 <a href="{{route('orders.category', ['category' => 300])}}">{{__('order.shipped')}}</a>
             @endif
         </li>
-        <li>
+        <li class="mb-1 bg-green-400 p-2">
             @if (request()->segment(count(request()->segments())) == 400)
                 <strong><a href="{{route('orders.category', ['category' => 400])}}">{{__('order.is-home')}}</a></strong>
             @else 
@@ -54,12 +54,12 @@
 <h1 class="text-center text-3xl">{{__('admin.orders')}}</h1>
 <ul id="order-list">
     @foreach ($orders as $order)
-    <li class="bg-gray-300  p-2 border border-black mb-1 flex justify-between">
-        <p>{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $order->created_at)->format('H:i | d.m.Y') }}</p>
-        <p>
+    <li class="grid grid-cols-1 md:grid-cols-3  p-2 border border-black mb-1  bg-gray-300">
+        <p class="flex justify-center">{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $order->created_at)->format('H:i | d.m.Y') }}</p>
+        <p class="flex justify-center">
             {{ $order->invoice_name}}
         </p>
-        <p>
+        <p class="flex justify-center md:justify-end">
             <a href="{{route('order.show', ['id' => $order->id])}}">{{__('admin.show-order')}}</a>
         </p>
     </li>
