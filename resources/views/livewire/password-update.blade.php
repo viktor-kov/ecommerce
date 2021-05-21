02dbe64c35472559a88209ac0183330b33ba851a
viktor-kov <viktor.kovacs02@gmail.com>
2021-05-21 11:39:17 +0200

    livewire add defer to models

--- resources/views/livewire/password-update.blade.php
+++ resources/views/livewire/password-update.blade.php
@@ -4,11 +4,11 @@
         @method('PUT')
         <aside class="mt-4">
             <p class="md:flex md:justify-between m-1">
-                <span class="break-words">Staré heslo:</span> <input type="password" name="current_password" class="w-full md:w-3/4 border border-black p-1" wire:model="current_password">
+                <span class="break-words">Staré heslo:</span> <input type="password" name="current_password" class="w-full md:w-3/4 border border-black p-1" wire:model.defer="current_password">
             </p>
             @error('current_password') <span class="text-red-500">{{ $message }}</span> @enderror
             <p class="md:flex md:justify-between m-1">
-                <span class="break-words">Nové heslo:</span> <input type="password" name="new_password" class="w-full md:w-3/4 border border-black p-1" wire:model="new_password">
+                <span class="break-words">Nové heslo:</span> <input type="password" name="new_password" class="w-full md:w-3/4 border border-black p-1" wire:model.defer="new_password">
             </p>
             @error('new_password') <span class="text-red-500">{{ $message }}</span> @enderror
             <p class="w-full">
