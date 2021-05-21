f499ec8b55b997466b692752f7c253dbe6bc9835
viktor-kov <viktor.kovacs02@gmail.com>
2021-05-21 14:32:05 +0200

    old data from input can be displayed

--- resources/views/guest/checkout.blade.php
+++ resources/views/guest/checkout.blade.php
@@ -15,42 +15,42 @@
     <section class="grid grid-cols-1 md:grid-cols-2 w-full h-min-screen">
         <section>
             <form action="{{route('checkout.store')}}" method="post" class="p-1" id="checkout_form">
-                @csrf 
+                @csrf
                 <p class="w-full block">
-                    {{__('profile.name')}} <input type="text" name="name" class="border border-black block outline-none w-full p-1" @if ($informations) value="{{$informations->name}}" placeholder="Name" @endif required>
+                    {{__('profile.name')}} <input type="text" name="name" value="@if ( old('name') ) {{old('name')}} @elseif ( $informations ) {{$informations->name}} @endif" class="border border-black block outline-none w-full p-1" placeholder="Name" required>
                 </p>
                 <p class="w-full block">
-                    {{__('profile.lastname')}} <input type="text" name="lastname" @if ($informations) value="{{$informations->lastname}}" placeholder="Lastname" @endif class="border border-black block outline-none w-full p-1" required>
+                    {{__('profile.lastname')}} <input type="text" name="lastname" value="@if ( old('lastname') ) {{old('lastname')}} @elseif ( $informations ) {{$informations->lastname}} @endif" class="border border-black block outline-none w-full p-1" placeholder="Lastname" required>
                 </p>
                 <p>
-                    {{__('profile.email')}} <input type="email" name="email" @if (auth()->user()) value="{{auth()->user()->email}}" placeholder="email@email.com" @endif class="border border-black block outline-none w-full p-1" required>
+                    {{__('profile.email')}} <input type="email" name="email" value="@if ( old('email') ) {{old('email')}} @elseif ( auth()->check() ) {{auth()->user()->email}} @endif" class="border border-black block outline-none w-full p-1" placeholder="email@email.com" required>
                 </p>
                 <p>
-                    {{__('profile.town')}} <input type="text" name="town" @if ($informations) value="{{$informations->town}}" @endif placeholder="Bratislava" class="border border-black block p-1 outline-none w-full" required>
+                    {{__('profile.town')}} <input type="text" name="town" value="@if ( old('town') ) {{old('town')}} @elseif ( $informations ) {{$informations->town}} @endif" class="border border-black block p-1 outline-none w-full" placeholder="Bratislava" required>
                 </p>
                 <p>
-                    {{__('profile.zip')}} <input type="text" name="psc" @if ($informations) value="{{$informations->psc}}" @endif placeholder="81103" class="border border-black block p-1 outline-none w-full" required>
+                    {{__('profile.zip')}} <input type="text" name="psc" value="@if ( old('psc') ) {{old('psc')}} @elseif ( $informations ) {{$informations->psc}} @endif" class="border border-black block p-1 outline-none w-full" placeholder="81103" required>
                 </p>
                 <p>
-                    {{__('profile.street')}} <input type="text" name="street" @if ($informations) value="{{$informations->street}}" @endif placeholder="Dúbravka" class="border border-black block p-1 outline-none w-full" required>
+                    {{__('profile.street')}} <input type="text" name="street" value="@if ( old('street') ) {{old('street')}} @elseif ( $informations ) {{$informations->street}} @endif" class="border border-black block p-1 outline-none w-full" placeholder="Dúbravka" required>
                 </p>
                 <p>
-                    {{__('profile.house-id')}} <input type="text" name="house_id" @if ($informations) value="{{$informations->house_id}}" @endif placeholder="456" class="border border-black block p-1 outline-none w-full" required>
+                    {{__('profile.house-id')}} <input type="text" name="house_id" value="@if ( old('house_id') ) {{old('house_id')}} @elseif ( $informations ) {{$informations->house_id}} @endif" class="border border-black block p-1 outline-none w-full" placeholder="456" required>
                 </p>
                 <p>
-                    {{__('profile.phone-number')}} <input type="text" name="phone_number" @if ($informations) value="{{$informations->phone_number}}" placeholder="0918250120" @endif class="border border-black block p-1 outline-none w-full" required>
+                    {{__('profile.phone-number')}} <input type="text" name="phone_number" value="@if ( old('phone_number') ) {{old('phone_number')}} @elseif ( $informations ) {{$informations->phone_number}} @endif" class="border border-black block p-1 outline-none w-full" placeholder="0918250120" required>
                 </p>
                 <p>
-                    {{__('checkout.card-number')}} <input type="text" name="card_number" class="border border-black block p-1 outline-none w-full" placeholder="4242 4242 4242 4242" required>
+                    {{__('checkout.card-number')}} <input type="text" name="card_number" class="border border-black block p-1 outline-none w-full" value="{{ old('card_number') }}" placeholder="4242 4242 4242 4242" required>
                 </p>
                 <p>
-                    {{__('checkout.card-exp-month')}} <input type="text" name="card_exp_month" class="border border-black block p-1 outline-none w-full" placeholder="01" required>
+                    {{__('checkout.card-exp-month')}} <input type="text" name="card_exp_month" class="border border-black block p-1 outline-none w-full" value="{{ old('card_exp_month') }}" placeholder="01" required>
                 </p>
                 <p>
-                    {{__('checkout.card-exp-year')}} <input type="text" name="card_exp_year" class="border border-black block p-1 outline-none w-full" placeholder="2025" required>
+                    {{__('checkout.card-exp-year')}} <input type="text" name="card_exp_year" class="border border-black block p-1 outline-none w-full" value="{{ old('card_exp_year') }}" placeholder="2025" required>
                 </p>
                 <p>
-                    {{__('checkout.card-cvc')}} <input type="text" name="card_cvc" class="border border-black block p-1 outline-none w-full" placeholder="456" required>
+                    {{__('checkout.card-cvc')}} <input type="text" name="card_cvc" class="border border-black block p-1 outline-none w-full" value="{{ old('card_cvc') }}" placeholder="456" required>
                 </p>
                 <p>
                     <button type="submit" id="submitButton" class="p-4 w-full text-white text-xl mt-4 inline-block bg-green-400 cursor-pointer">{{__('checkout.total-sum')}} {{Cart::subtotal()}}€</button>
