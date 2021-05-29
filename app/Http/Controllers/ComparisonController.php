27a6974072b1e6a3afafa833c12daa20ec2fa0bf
viktor-kov <viktor.kovacs02@gmail.com>
2021-05-29 18:30:19 +0200

    show and delete method for comparison

--- app/Http/Controllers/ComparisonController.php
+++ app/Http/Controllers/ComparisonController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\ProductComparison;
+use App\Services\ProductService;
 use Illuminate\Http\Request;
 
 class ComparisonController extends Controller
 {
-    public function showComparison() {
-        
+    public function showComparison(ProductService $productService) {
+        if(! session('comparison.comparison_id')) {
+            abort(404);
+        }
+
+        $comparedProducts = [];
+
+        if(session('comparison.products.product_1')) {
+            $product = Product::find(session('comparison.products.product_1'));
+            $productOneSpecifications = $productService->showProductSpecifications($product->category, $product->id);
+
+            $productOne['productOne']['product']  = $product;
+            $productOne['productOne']['specificationsView'] = $productOneSpecifications['product_view'];
+            $productOne['productOne']['specifications'] = $productOneSpecifications['product_spect'];
+
+            $comparedProducts = array_merge($comparedProducts, $productOne);
+        }
+
+        if(session('comparison.products.product_2')) {
+            $product = Product::find(session('comparison.products.product_2'));
+            $productTwoSpecifications = $productService->showProductSpecifications($product->category, $product->id);
+
+            $productTwo['productTwo']['product'] = $product;
+            $productTwo['productTwo']['specificationsView'] = $productTwoSpecifications['product_view'];
+            $productTwo['productTwo']['specifications'] = $productTwoSpecifications['product_spect'];
+
+            $comparedProducts = array_merge($comparedProducts, $productTwo);
+        }
+
+        return view('comparison.productComparison', compact('comparedProducts'));
     }
 
     public function addToComparison(Request $request) {
@@ -43,5 +73,13 @@ class ComparisonController extends Controller
 
             return redirect()->route('comparison.show');
         }
+
+        return redirect()->route('comparison.show');
+    }
+
+    public function deleteFromComparison($product_field) {
+        session()->forget('comparison.products.' . $product_field);
+
+        return back();
     }
 }
