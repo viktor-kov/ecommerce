6be5bb68ca81a74c3f68536cec148d883066776e
viktor-kov <viktor.kovacs02@gmail.com>
2021-05-29 22:10:33 +0200

    html change

--- resources/views/layouts/adminlayout.blade.php
+++ resources/views/layouts/adminlayout.blade.php
@@ -22,14 +22,15 @@
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
         @yield('extra-js')
     </head>
-    <header class="h-20 md:h-10 bg-black">
-        @include('header')
-    </header>
-    @if ($errors->any())
-        @include('messages.validation-error')
-    @endif
-    @yield('messages')
     <body class="container font-sans antialiased mx-auto">
+        <header class="h-20 md:h-10 bg-black">
+            @include('header')
+        </header>
+        @include('search-input')
+        @if ($errors->any())
+            @include('messages.validation-error')
+        @endif
+        @yield('messages')
         @yield('navigation')
         <main>
             <section>
@@ -39,3 +40,4 @@
         @livewireScripts
         @yield('footer-js')
     </body>
+</html>
