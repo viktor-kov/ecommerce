419b45aa5b0a79c9be497b45c34862f0555bd04a
viktor-kov <viktor.kovacs02@gmail.com>
2021-06-20 15:13:00 +0200

    product category fix

--- resources/views/product-preview/productShowSection.blade.php
+++ resources/views/product-preview/productShowSection.blade.php
@@ -1,11 +1,11 @@
 <li class="product-container bg-white shadow-xl h-96 border w-90 p-2">
-    <a href="{{route('product.show', ['id' => $product->category, 'slug' => $product->slug])}}">
+    <a href="{{route('product.show', ['category_id' => $product->getCategory->category_name, 'slug' => $product->slug])}}">
         <div class="product-img">
             <img src="{{asset('img/'.$product->photo_path)}}" alt="{{$product->name}}" class="h-64 mx-auto my-auto transform scale-75">
         </div>
         <div class="product-information">
             <h2 class="h-10 text-lg font-semibold">
-                <a href="{{route('product.show', ['id' => $product->category, 'slug' => $product->slug])}}">{{$product->name}}</a>
+                <a href="{{route('product.show', ['category_id' => $product->getCategory->category_name, 'slug' => $product->slug])}}">{{$product->name}}</a>
             </h2>
             <div class="product-price items-center mt-4">
                 @if ($product->amount->product_amount > 0)
